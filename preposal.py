c09a52eed2bc1f6f62123ecc2bbe8eb095f3c167
landonterrell-159 <landonterrell159@gmail.com>
2025-11-01 21:07:31 -0400

    Add files via upload

---
+++ preposal.py
@@ -0,0 +1,14 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+
+-A Tron Game 
+- A Horor Game
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+- Solly
+
+## Do you have any questions of your own?
+
+- No 
